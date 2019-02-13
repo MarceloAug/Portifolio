54b0d374af007d7fdd98a4c1ed26a855751acd9f
marcelo augusto <marcelomab09@gmail.com>
2019-02-13 21:28:07 -0200

    Alterações no layout

--- resources/views/Empresas/EmpresasListar.blade.php
+++ resources/views/Empresas/EmpresasListar.blade.php
@@ -4,77 +4,72 @@
 
     @if(empty($empresas))
         <div class="alert alert-danger">
-            Você não tem nenhum produto cadastrado.
+            Vocï¿½ nï¿½o tem nenhum produto cadastrado.
         </div>
 
     @else
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Listagem de empresas</h3>
-                        <form action="/empresas/Filtra" method = "GET">
-                            <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-                            <div class="box-tools">
-                                <div class="input-group input-group-sm">
-                                    <div class = "lines">
-                                        <div>
-                                            <div class="form-row">
-                                                <div class="col">
-                                                <select name="filtro" class="form-control">
-                                                    <option value="ID">ID</option>
-                                                    <option value="Nome">Nome</option>
-                                                </select>
-                                            </div>
-                                            <div class="col">
-                                                <div class="input-group input-group-sm" style="width: 150px;">
-                                                     <input type="text" name="campo" class="form-control pull-right" placeholder="Search">
-
-                                                <div class="input-group-btn">
-                                                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                                </div>
-                                            </div>
-                                            </div>
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Listagem de empresas</h3>
+                <form action="/empresas/Filtra" method = "GET">
+                    <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                    <div class="box-tools">
+                        <div class="input-group input-group-sm">
+                            <div>
+                                <div>
+                                    <div class="form-row">
+                                        <div class="col">
+                                        <select name="filtro" class="form-control">
+                                            <option value="ID">ID</option>
+                                            <option value="Nome">Nome</option>
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <div class="input-group input-group-sm" style="width: 150px;">
+                                             <input type="text" name="campo" class="form-control pull-left" placeholder="Search"
+                                        <div class="input-group-btn">
+                                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                                         </div>
+                                    </div>
                                     </div>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                    <div class="box-body">
-                        <table id="listaempresas" class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>Descricao</th>
-                                    <th>Remover</th>
-                                    <th>Editar</th>
-                                </tr>
-                            </thead>
-                          <tbody>
-                                @foreach ($empresas as $e)
-                                <tr>
-                                    <td> {{$e->ID}} </td>
-                                    <td> {{$e->nome}} </td>
-                                    <td> {{$e->Descricao}} </td>
-                                    <td>
-                                        <a href="/empresas/remove/{{$e->ID}}">
-                                            <span class="glyphicon glyphicon-trash"></span>
-                                        </a>
-                                    </td>
-                                
-                                      <td>
-                                        <a href="/empresas/FormEditar/{{$e->ID}}">
-                                            <i class="glyphicon glyphicon-pencil"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                          </tbody>
-                        </table>
-                    </div>
-                </div>
+                </form>
+            </div>
+            <div class="box-body">
+                <table id="listaempresas" class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Descricao</th>
+                            <th>Remover</th>
+                            <th>Editar</th>
+                        </tr>
+                    </thead>
+                  <tbody>
+                        @foreach ($empresas as $e)
+                        <tr>
+                            <td> {{$e->ID}} </td>
+                            <td> {{$e->nome}} </td>
+                            <td> {{$e->Descricao}} </td>
+                            <td>
+                                <a href="/empresas/remove/{{$e->ID}}">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                </a>
+                            </td>
+                        
+                              <td>
+                                <a href="/empresas/FormEditar/{{$e->ID}}">
+                                    <i class="glyphicon glyphicon-pencil"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                  </tbody>
+                </table>
             </div>
         </div>
     @endif
--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -10,13 +10,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <title>AdminLTE 2 | Starter</title>
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/custom.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/AdminLTE.min.css">
     <link rel="stylesheet" href="/css/skin-blue.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
     <body class="hold-transition skin-blue sidebar-mini">
-        <div class="wrapper">
+        <div class="wrapper" style= "width:100%">
 
                 <header class="main-header">
                         <a href="index2.html" class="logo">
@@ -25,22 +24,34 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                          <!-- Header Navbar -->
                         <nav class="navbar navbar-static-top" role="navigation">
-
+                            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                            <span class="sr-only">Toggle navigation</span>
+                        </a>
                         </nav>
                 </header>
 
                 <aside class="main-sidebar">
-                        <section class="sidebar">
-                            <ul class="sidebar-menu" data-widget="tree">
-                                <li><a href="/empresas">Listagem</a></li>
-                                <li><a href="/empresas/FormAdicionar">Cadastrar Empresa</a></li>
-                            </ul>
-                        </section>
+                    <section class="sidebar">
+                        <ul class="sidebar-menu" data-widget="tree">
+                            <li class="header">HEADER</li>
+                            <li class="treeview">
+                                <a href="#"><i class="fa fa-link"></i> <span>Empresas</span>
+                                    <span class="pull-right-container">
+                                        <i class="fa fa-angle-left pull-right"></i>
+                                    </span>
+                                </a>
+                                <ul class="treeview-menu">
+                                    <li><a href="/empresas/FormAdicionar">Cadastrar Empresa</a></li>
+                                    <li><a href="/empresas">Listagem</a></li>
+                                </ul>
+                             </li>
+                        </ul>
+                    </section>
                 </aside>
         
                 <div class="content-wrapper">
                         <!-- Content Header (Page header) -->
-                        <section class="content-header">
+                        <section class="content container-fluid">
                             @yield('conteudo')
                         </section>
                 </div>
@@ -50,5 +61,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </div>
             </footer>
         </div>
+
+        <!-- jQuery 3 -->
+<script src="/js/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/js/adminlte.min.js"></script>
     </body>
 </html>
